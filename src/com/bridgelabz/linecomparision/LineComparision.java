d9a98258bb268c8bfe5587997c0f0c347b839e44
PREETI2096 <preetirg96@gmail.com>
2023-01-05 00:26:41 +0530

    [Preeti]: added uc1 calculate length of line

---
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linecomparision;
+import java.util.Scanner;
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison computation program!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter coordinates of first point: ");
+		int x1 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		
+		System.out.println("Enter coordinates of second point: ");
+		int x2 = scanner.nextInt();
+		int y2 = scanner.nextInt();
+		
+		double length = Math.sqrt(Math.pow((x2- x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println("Length of line = " + length);
+	}
+}
