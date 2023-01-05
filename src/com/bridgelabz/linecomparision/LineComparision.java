1a5db93cdce115c9e09f38368b5825ef564c76d7
PREETI2096 <preetirg96@gmail.com>
2023-01-05 14:51:44 +0530

    [Preeti]: added UC2 equality check of two lines

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -4,16 +4,24 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison computation program!");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter coordinates of first point: ");
-		int x1 = scanner.nextInt();
-		int y1 = scanner.nextInt();
+		int x1 = 20 , y1 = 6 ;
+		int x2 = 8 , y2 = 7;
+		int x3 = 6 , y3 = 9;
+		int x4 = 25 , y4 = 45;
 		
-		System.out.println("Enter coordinates of second point: ");
-		int x2 = scanner.nextInt();
-		int y2 = scanner.nextInt();
+		Double length1 = Math.sqrt(Math.pow(x2- x1, 2) + Math.pow(y2 - y1, 2));
+		Double length2  = Math.sqrt(Math.pow(x4- x3, 2) + Math.pow(y4 - y3, 2));
 		
-		double length = Math.sqrt(Math.pow((x2- x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println("Length of line = " + length);
+		
+		System.out.println("Length of first line = " + length1);
+		
+		System.out.println("Length of second line = " + length2);
+		
+		System.out.println(length1.equals(length2));
+		
+		if(length1.equals(length2)) 
+			System.out.println("Both lines have equal length");
+		else 
+			System.out.println("Both lines have  different length");
+		}
 	}
-}
