685952f09619e894e8b50678e4a7bb4a8f918db1
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 17:20:45 +0530

    Added : length calculation of line program

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -4,6 +4,7 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison computation program!");
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter coordinates of first point: ");
 		int x1 = scanner.nextInt();
