110dee64584fe78b79821c3a891bae1ddb3137ee
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 17:30:21 +0530

    Added : equality checking program

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class LineComparision {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Line Comparison computation program!");
 		int x1 = 20 , y1 = 6 ;
 		int x2 = 8 , y2 = 7;
